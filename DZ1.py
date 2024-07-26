2d607e3b7ed9542c7ebd6f90beab25d9ba8d1aa2
NikitaBaranov43 <fast.flou@gmail.com>
2024-07-26 16:54:07 +0300

    Add files via upload

---
+++ DZ1.py
@@ -0,0 +1,9 @@
+#"1st program".
+print(9 ** 0.5 * 5 )
+# 2nd program
+print(9.99 > 9.98 and 1000 != 1000.1)
+#3rd program
+print(1234 // 10 , 123 % 100 , 5678 // 10 , 567 % 100 , 23 + 67)
+#4th program
+print(1342 // 100  == 4213 % 100 )
+
